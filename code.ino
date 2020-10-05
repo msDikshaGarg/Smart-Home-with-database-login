41525e4d622f1bf39edad61953fda6503263389a
msDikshaGarg <70652158+msDikshaGarg@users.noreply.github.com>
2020-10-05 11:59:34 -0400

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,71 @@
+#include <Servo.h>
+#include <EEPROM.h>
+
+Servo servo;
+char state;
+
+void setup() {
+  
+  servo.attach(7);
+
+  if(EEPROM.read(0) == 1)
+  {                      
+    servo.write(70); 
+    delay(200);
+  }
+  else if(EEPROM.read(0) == 2)
+  {
+    servo.write(120); 
+    delay(200);
+  }
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+ 
+  if(Serial.available() > 0)
+  {
+    char data;
+    data = Serial.read();
+    Serial.print(data);
+    switch(data)
+    {
+      case '1': 
+        if(EEPROM.read(0) == 1) 
+        {
+          EEPROM.write(0, 2);
+          Serial.print("3");
+          for(int a = 70; a <= 120; a++) 
+          {
+            servo.write(a);
+            delay(15);
+            Serial.println(servo.read());
+          }
+        }
+        else if(EEPROM.read(0) == 2)
+        {
+          EEPROM.write(0, 1); 
+          Serial.print("4"); 
+          for(int x = 120; x >= 70; x--)
+          {
+            servo.write(x);
+            delay(15);
+          }
+        }
+        break;
+      case '3': 
+     
+        if(EEPROM.read(0) == 1)
+        {
+          Serial.print("4");
+        }
+        else if(EEPROM.read(0) == 2)
+        {
+          Serial.print("3");
+        }
+        break;
+    }
+  }
+  
+}
---
+++ diksha_iot.py
@@ -0,0 +1,32 @@
+import serial
+import time
+import pyrebase
+#import matplotlib as plt
+
+value_time=[]
+value_read=[]
+config = {
+    "apiKey": "AIzaSyDhI9M8P6yXayigbm8sGk_JvDH2lP-m_1s",
+    "authDomain": "dikshaiot-79cdd.firebaseapp.com",
+    "databaseURL": "https://dikshaiot-79cdd.firebaseio.com",
+    "projectId": "dikshaiot-79cdd",
+    "storageBucket": "dikshaiot-79cdd.appspot.com",
+    "messagingSenderId": "486388324579"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+#time_cc=int(time.time())
+#obj = {"timestamp":time_cc,"reading":200}
+#db.child("waterlevel").push(obj)
+import serial
+ser = serial.Serial('COM5', 9600)
+while True:
+    rdata = ser.readline().decode('utf-8')
+    value_read.append(rintd)
+    time_cc=int(time.time())
+    value_time.append(time_cc)
+    #plt.plot(value_time,value_read)
+    #plt.show()
+    obj = {"timestamp":time_cc,"reading":rdata}
+    db.child("motor").push(obj)
